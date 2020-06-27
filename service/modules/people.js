9488ba8cf30beee89e83cf19b5b4c5e294da6221
Elberth Cabrales <elberthcabrales@gmail.com>
2020-06-26 20:02:08 -0600

    [TICKETID] Add catch errors in people module

--- service/modules/people.js
+++ service/modules/people.js
@@ -2,25 +2,29 @@ module.exports = ({ axios, redis}) => ({
     get: async (req, res) => { 
         const {get, set} = redis.instance;
         const { key, feature_redis } = redis
-        if(feature_redis){
-            const value = await get(key)
-            if(value) return res.status(200).send(value)
-        }      
-        const {data: data} = await axios.get(
-            "/people.json"
-        )
-        if(data){
-            data.data.forEach(({id, first_name, last_name, full_email_address, title},i) =>{
-                email = full_email_address
-                email = email.substring(email.indexOf('<')+1, email.length-1);
-                data.data[i] = {id:id,
-                    name: first_name + " " + last_name,
-                    email: email,
-                    job: title
-                }
-            })
-            if(feature_redis) await set(key, data.data)
-            return res.status(200).send(data.data)
+        try {
+            if(feature_redis){
+                const value = await get(key)
+                if(value) return res.status(200).send(value)
+            }      
+            const {data: data} = await axios.get(
+                "/people.json"
+            )
+            if(data){
+                data.data.forEach(({id, first_name, last_name, full_email_address, title},i) =>{
+                    email = full_email_address
+                    email = email.substring(email.indexOf('<')+1, email.length-1);
+                    data.data[i] = {id:id,
+                        name: first_name + " " + last_name,
+                        email: email,
+                        job: title
+                    }
+                })
+                if(feature_redis) await set(key, data.data)
+                return res.status(200).send(data.data)
+            }
+        } catch (error) {
+            console.log("module.people.error",error)
         }
         res.status(400)
     }
